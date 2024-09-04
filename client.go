c39ca0e538224babe567292e01e9fa8d815099de
jdnvn <joe@donovan.to>
2024-09-04 00:22:22 -0400

    add ui and refactor some of the client code into functions

--- client.go
+++ client.go
@@ -3,18 +3,91 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
 
+	"github.com/gdamore/tcell/v2"
 	"github.com/gorilla/websocket"
+	"github.com/rivo/tview"
 )
 
 const PublicServerHost = "lvh.me"
+
+type UI struct {
+	app   *tview.Application
+	table *tview.Table
+	rows  int
+}
+
+func createUI(id string, localServerHost string) *UI {
+	app := tview.NewApplication()
+
+	title := tview.NewTextView()
+	title.SetText(" [blue]jgrok[-]")
+	title.SetDynamicColors(true)
+
+	forwarding := tview.NewTextView()
+	forwarding.SetText(fmt.Sprintf(" %s.%s [yellow]->[-] %s", string(id), PublicServerHost, localServerHost))
+	forwarding.SetDynamicColors(true)
+
+	spacer := tview.NewTextView()
+
+	table := tview.NewTable()
+
+	requests := tview.NewFlex()
+	requests.SetTitle("requests")
+	requests.SetTitleAlign(tview.AlignLeft)
+	requests.SetDirection(tview.FlexRow)
+	requests.SetBorder(true)
+	requests.AddItem(spacer, 1, 0, false)
+	requests.AddItem(table, 0, 1, false)
+
+	container := tview.NewFlex()
+	container.SetDirection(tview.FlexRow)
+	container.AddItem(title, 1, 0, false)
+	container.AddItem(spacer, 1, 0, false)
+	container.AddItem(forwarding, 1, 0, false)
+	container.AddItem(spacer, 1, 0, false)
+	container.AddItem(requests, 0, 2, false)
+
+	app.SetRoot(container, true)
+
+	ui := UI{app: app, table: table, rows: 0}
+
+	return &ui
+}
+
+func (ui *UI) addTableRow(httpMethod string, path string, statusCode int) {
+	ui.table.SetCell(ui.rows, 0, tview.NewTableCell(time.Now().Format(" 15:04:05.456")))
+	ui.table.SetCell(ui.rows, 1, tview.NewTableCell(httpMethod))
+	ui.table.SetCell(ui.rows, 2, tview.NewTableCell(path))
+	statusCodeCell := tview.NewTableCell(strconv.Itoa(statusCode))
+	if statusCode >= 400 {
+		statusCodeCell.SetTextColor(tcell.ColorRed)
+	}
+	ui.table.SetCell(ui.rows, 3, statusCodeCell)
+	ui.app.Draw()
+	ui.rows += 1
+}
+
+func connectToServer() (*websocket.Conn, error) {
+	u := url.URL{Scheme: "ws", Host: PublicServerHost}
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	return conn, err
+}
+
+func deserializeMessage(message []byte) (*ForwardedRequest, error) {
+	var forwardedReq ForwardedRequest
+	err := json.Unmarshal(message, &forwardedReq)
+	return &forwardedReq, err
+}
 
 func main() {
 	args := os.Args
@@ -32,75 +105,84 @@ func main() {
 	localServerHost := "http://localhost:" + localPort
 
 	// connect to the websocket server
-	u := url.URL{Scheme: "ws", Host: PublicServerHost}
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	conn, err := connectToServer()
 	if err != nil {
-		log.Fatal("Dial error:", err)
+		log.Fatal("error connecting to jgrok server", err)
 	}
-	defer c.Close()
 
-	// setup a channel to catch interrupt signals for clean shutdown
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-
-	// read ID and print to user
-	_, id, err := c.ReadMessage()
+	// read ID to be used as the subdomain
+	_, id, err := conn.ReadMessage()
 	if err != nil {
-		log.Println("error receiving id from server", err)
-		return
+		log.Fatal("error connecting to jgrok server", err)
 	}
-	log.Printf("%s.%s -> %s", string(id), PublicServerHost, localServerHost)
+	defer conn.Close()
 
 	client := &http.Client{}
 
-	// start listening for messages from socket
+	ui := createUI(string(id), localServerHost)
+
+	// setup channels for clean shutdown
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
 	done := make(chan struct{})
+
+	// start the ui in its own thread
 	go func() {
 		defer close(done)
+
+		if err := ui.app.Run(); err != nil {
+			panic(err)
+		}
+	}()
+
+	// start listening for messages from websocket
+	go func() {
+		defer close(done)
+
 		for {
-			_, message, err := c.ReadMessage()
+			_, message, err := conn.ReadMessage()
 			if err != nil {
 				log.Println("Read error:", err)
+				continue
+			}
+
+			forwardedReq, err := deserializeMessage(message)
+			if err != nil {
+				log.Println("Error deserializing message:", err)
+				continue
+			}
+
+			bodyReader := bytes.NewReader([]byte(forwardedReq.Body))
+			req, _ := http.NewRequest(forwardedReq.Method, localServerHost+forwardedReq.URL, bodyReader)
+
+			for key, values := range forwardedReq.Headers {
+				for _, value := range values {
+					req.Header.Add(key, value)
+				}
+			}
+
+			resp, _ := client.Do(req)
+			body, _ := io.ReadAll(resp.Body)
+
+			forwardedResp := ForwardedResponse{
+				StatusCode: resp.StatusCode,
+				Headers:    resp.Header,
+				Body:       body,
+			}
+
+			responseJson, err := json.Marshal(forwardedResp)
+			if err != nil {
+				log.Println("Error marshaling response:", err)
 				return
 			}
 
-			log.Printf("Received: %s, forwarding to local server...", message)
-
-			var forwardedReq ForwardedRequest
-			parseErr := json.Unmarshal(message, &forwardedReq)
-			if parseErr != nil {
-				log.Println("Error parsing request:", parseErr)
-			} else {
-				bodyReader := bytes.NewReader([]byte(forwardedReq.Body))
-				req, _ := http.NewRequest(forwardedReq.Method, localServerHost+forwardedReq.URL, bodyReader)
-
-				for key, values := range forwardedReq.Headers {
-					for _, value := range values {
-						req.Header.Add(key, value)
-					}
-				}
-
-				resp, _ := client.Do(req)
-				body, _ := io.ReadAll(resp.Body)
-
-				forwardedResp := ForwardedResponse{
-					StatusCode: resp.StatusCode,
-					Headers:    resp.Header,
-					Body:       body,
-				}
-
-				responseJson, err := json.Marshal(forwardedResp)
-				if err != nil {
-					log.Println("Error marshaling response:", err)
-					return
-				}
-
-				err = c.WriteMessage(websocket.TextMessage, responseJson)
-				if err != nil {
-					log.Println("Error writing response to socket:", err)
-					return
-				}
+			err = conn.WriteMessage(websocket.TextMessage, responseJson)
+			if err != nil {
+				log.Println("Error writing response to socket:", err)
+				return
 			}
+
+			ui.addTableRow(forwardedReq.Method, forwardedReq.URL, forwardedResp.StatusCode)
 		}
 	}()
 
@@ -108,16 +190,17 @@ func main() {
 	for {
 		select {
 		case <-done:
+			ui.app.Stop()
 			return
 		case <-interrupt:
-			log.Println("Interrupt received, closing connection...")
+			ui.app.Stop()
 
 			// close the websocket connection gracefully
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
-				log.Println("Close error:", err)
-				return
+				log.Fatal("Close error:", err)
 			}
+
 			select {
 			case <-done:
 			case <-time.After(time.Second):
--- go.mod
+++ go.mod
@@ -6,14 +6,20 @@ require github.com/gorilla/websocket v1.5.3
 
 require (
 	github.com/brianvoe/gofakeit/v6 v6.28.0 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/tview v0.0.0-20240818110301-fd649dbf1223 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/yuin/goldmark v1.4.13 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/mod v0.8.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/term v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
 	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7 // indirect
 )
